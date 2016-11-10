db78d129ae87fb8e83a71b83fe9f6cec312847c8
Ian Agpawa <agpawaji@gmail.com>
2016-11-09 20:24:07 -0500

    small changes

--- PostPage.py
+++ PostPage.py
@@ -1,13 +1,3 @@
-import os
-import jinja2
-import webapp2
-
-
-from User import User
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                                autoescape = True)
 
 from Handler import Handler
 import Post
