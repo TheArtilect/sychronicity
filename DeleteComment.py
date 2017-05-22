5a90a7159eacc6756af79bed2436826eeec2693c
Ian Agpawa <agpawaji@gmail.com>
2017-05-22 16:27:49 -0400

    refactor: Update delete comment handler for post

--- DeleteComment.py
+++ DeleteComment.py
@@ -2,6 +2,7 @@
 from Handler import Handler
 import Post
 import Comment
+import time
 
 from google.appengine.ext import db
 
@@ -24,3 +25,21 @@ class DeleteComment(Handler):
             return self.redirect("/")
 
         return self.render("delete_comment.html", comment = comment)
+
+
+    def post(self, comment_id):
+        comment_key = db.Key.from_path("Comment", int(comment_id), parent=Comment.comment_key())
+        comment = db.get(comment_key)
+        post_id = int(comment.p_id)
+
+        if self.request.get("cancel_delete_comment"):
+            return self.redirect("/%s" % post_id)
+
+        if self.request.get('delete_comment'):
+            post_key = db.Key.from_path("Post", post_id, parent=Post.blog_key())
+            post = db.get(post_key)
+            post.comments.remove(str(comment_key))
+            post.put()
+            db.delete(comment_key)
+            time.sleep(0.1)
+            return self.redirect("/%s" % post_id)
